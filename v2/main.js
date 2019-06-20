8fbdcf690a5be89ebc487e7214633b0a0f2327a5
teresasempere <teresa3tc@gmail.com>
2019-06-20 12:03:14 +0200

    Ging header and footer

--- v2/main.js
+++ v2/main.js
@@ -1,9 +1,24 @@
-/*$(window).scroll(function() {
-    if ($(window).scrollTop() > 100) {
-        $('.float_navBar').removeClass('hidden');
-    } else {
-        $('.navBar').addClass('hidden');
-    }
-});*/
+window.onscroll = function () {
+    myFunction();
+};
+document.getElementById("parallax").onscroll = function () {
+    myFunction2();
+};
 
-console.log("Hola");
+function myFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("header").className = "navBar border_navbar";
+    } else {
+        document.getElementById("header").className = "navBar";
+    }
+};
+
+
+function myFunction2() {
+    if (document.getElementById("parallax").scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("header_home").className = "navBar border_navbar";
+
+    } else {
+        document.getElementById("header_home").className = "navBar";
+    }
+};
