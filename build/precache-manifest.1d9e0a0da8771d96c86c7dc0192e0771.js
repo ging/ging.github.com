fedf752bb5c9931bc8c3dac1de6eb753160b92ac
Sonsoles López Pernas <sonsoleslp@hotmail.com>
2019-11-06 23:16:08 +0100

    Add build

---
+++ build/precache-manifest.1d9e0a0da8771d96c86c7dc0192e0771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7801dd0eb3d035d15394d8fcd0c03850",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "b0ecb4ccffde33803f3c",
+    "url": "/build/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "616d75d0f9a65b5f7362",
+    "url": "/build/static/css/main.58e2f162.chunk.css"
+  },
+  {
+    "revision": "b0ecb4ccffde33803f3c",
+    "url": "/build/static/js/2.364e11e9.chunk.js"
+  },
+  {
+    "revision": "616d75d0f9a65b5f7362",
+    "url": "/build/static/js/main.95b82364.chunk.js"
+  },
+  {
+    "revision": "84f50fcbb432ea3e1225",
+    "url": "/build/static/js/runtime~main.ca5d9680.js"
+  }
+]);
