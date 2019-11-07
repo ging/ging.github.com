93fef681fabf8e3424d3197e600214f02a0f9247
aalonsog <aalonsog@dit.upm.es>
2019-11-07 09:47:28 +0100

    Update info

---
+++ build/precache-manifest.86ceae4ec30e225c28fa9b9233fd6222.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aec9d9eb149a2d1fecc674c1a3ae146",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "897403832b4a28b19861",
+    "url": "/build/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "e436bbe38b12a71dcc3c",
+    "url": "/build/static/css/main.c8ff1f80.chunk.css"
+  },
+  {
+    "revision": "897403832b4a28b19861",
+    "url": "/build/static/js/2.3fcf8b42.chunk.js"
+  },
+  {
+    "revision": "e436bbe38b12a71dcc3c",
+    "url": "/build/static/js/main.2d73d56c.chunk.js"
+  },
+  {
+    "revision": "e034e5d87a870accec20",
+    "url": "/build/static/js/runtime~main.a4c46af1.js"
+  }
+]);
