ac8cc830107937f579d891dcf0d4e3699a20ef7b
Ana Marin <ana.marin.e@gmail.com>
2019-11-08 15:57:20 +0100

    Responsive fixed, team member added

---
+++ build/precache-manifest.5fd848b053f4ce24544767a36f1dde3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc576da8460d6ef060995c9348d48b9",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "b0ecb4ccffde33803f3c",
+    "url": "/build/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "597718e27e4fe391b894",
+    "url": "/build/static/css/main.edf3e385.chunk.css"
+  },
+  {
+    "revision": "b0ecb4ccffde33803f3c",
+    "url": "/build/static/js/2.364e11e9.chunk.js"
+  },
+  {
+    "revision": "597718e27e4fe391b894",
+    "url": "/build/static/js/main.0e84debb.chunk.js"
+  },
+  {
+    "revision": "84f50fcbb432ea3e1225",
+    "url": "/build/static/js/runtime~main.ca5d9680.js"
+  }
+]);
