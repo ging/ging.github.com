6648a60e87ac3598a37843e0229377b6777b9309
javicond3 <4b3javi@gmail.com>
2021-08-02 15:14:37 +0200

    added publication

---
+++ build/precache-manifest.916b3d83588a8d2ae04c52598a2e8681.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0808f7f6006ab7dc13d7e82aa20d86",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "b316435fb19074d85c62",
+    "url": "/build/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "5bd194e79c74a1beb14f",
+    "url": "/build/static/css/main.56881c59.chunk.css"
+  },
+  {
+    "revision": "b316435fb19074d85c62",
+    "url": "/build/static/js/2.275bc5e4.chunk.js"
+  },
+  {
+    "revision": "291e0d2bc65cd0d19fff9204b8020a70",
+    "url": "/build/static/js/2.275bc5e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd194e79c74a1beb14f",
+    "url": "/build/static/js/main.9c8a1e30.chunk.js"
+  },
+  {
+    "revision": "5497c96086e85a1867c8",
+    "url": "/build/static/js/runtime-main.5455ec96.js"
+  }
+]);
