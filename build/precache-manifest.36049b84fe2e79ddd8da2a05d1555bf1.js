88234a77776fd803e1f3d69c98c6169fb2aca1b7
Álvaro Alonso <aalonsog@dit.upm.es>
2023-07-10 12:10:55 +0200

    Update info

---
+++ build/precache-manifest.36049b84fe2e79ddd8da2a05d1555bf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2555175e0897f02c42f1b695e1660a65",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "b316435fb19074d85c62",
+    "url": "/build/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "c8dff2d67a1efb9124ed",
+    "url": "/build/static/css/main.e7ad3e3f.chunk.css"
+  },
+  {
+    "revision": "b316435fb19074d85c62",
+    "url": "/build/static/js/2.275bc5e4.chunk.js"
+  },
+  {
+    "revision": "291e0d2bc65cd0d19fff9204b8020a70",
+    "url": "/build/static/js/2.275bc5e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8dff2d67a1efb9124ed",
+    "url": "/build/static/js/main.bf780c41.chunk.js"
+  },
+  {
+    "revision": "5497c96086e85a1867c8",
+    "url": "/build/static/js/runtime-main.5455ec96.js"
+  }
+]);
