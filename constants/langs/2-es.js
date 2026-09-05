ea6368452e2837fab7e696969df113b7e908d1fd
javicond3IA <javier.wachetes@gmail.com>
2026-09-05 17:32:48 +0300

    Add Spanish labels for Profesor Permanente Laboral (es/en)

---
+++ constants/langs/2-es.js
@@ -0,0 +1,195 @@
+"use client";
+
+export const es = {
+  nav: {
+    home: "Home",
+    about: "Sobre [proyecto]",
+    team: "Equipo",
+    research: "Publicaciones",
+    contact: "Contacto",
+    designSystem: "Sistema de diseño",
+    documentation: "Documentación",
+    projects: "Proyectos",
+  },
+  header: {
+    publicationstab: "Publicaciones",
+  },
+  front: {
+    title: "Grupo de Internet de Nueva Generación",
+    description:
+      "Un grupo de investigación del Departamento de Ingeniería Telemática (DIT) de la Universidad Politécnica de Madrid (UPM). Nuestro trabajo está centrado en la innovación educativa, los espacios de datos, las redes, las videoconferencias y la inteligencia artificial.",
+      "action-button": "Ver líneas de investigación",
+    section1Title: "Sobre nosotros",
+      "section1Description": "Nuestros principales intereses de investigación se centran en tecnologías de espacios de datos, IA generativa, e-Learning y sistemas de videoconferencia. Tenemos una amplia experiencia en diseño de protocolos y especificaciones así como en el desarrollo de componentes software y casos de uso en estas áreas. Participamos en proyectos de investigación competitivos de financiación pública a nivel nacional y europeo y en convenios de colaboración con empresas.",
+    section1Subtitle: "Principios del proyecto",
+       statistics: {
+      papers: "publicaciones",
+      citations: "citas",
+      projects: "proyectos",
+    },
+    // latestPublicationsTitle: "Últimas publicaciones",
+    // latestPublicationsButton: "Ver todas las publicaciones",
+    ResearchLines: {
+      sectionTitle: "Líneas de investigación",
+       "button": "Ver proyectos",
+      ResearchLine1: {
+        ResearchLineTitle: "Datos",
+        ResearchLineBody:
+          "Esta línea aborda el estudio y desarrollo de tecnologías avanzadas para el procesamiento, gestión y análisis de datos, incluyendo espacios de datos y gemelos digitales.",
+      },
+      ResearchLine2: {
+        ResearchLineTitle: "Tecnología educativa",
+        ResearchLineBody:
+          "Esta línea se centra en el diseño y desarrollo de tecnologías educativas avanzadas, incluyendo plataformas de aprendizaje, herramientas de autor, ia en educación y escape rooms educativas.",
+      },
+      ResearchLine3: {
+        ResearchLineTitle: "Inteligencia Artificial",
+        ResearchLineBody:
+          "Esta línea se enfoca en la investigación y desarrollo de aplicaciones basadas en inteligencia artificial, incluyendo la evaluación de modelos de lenguaje de gran escala (LLMs), ajuste fino (fine-tuning), recuperación aumentada por generación (RAG).",
+      },
+      ResearchLine4: {
+        ResearchLineTitle: "Videoconferencia",
+        ResearchLineBody:
+          "Esta línea se centra en el desarrollo de herramientas y plataformas de videoconferencia, como Isabel y Licode, así como en la aplicación de estas tecnologías a la interpretación y la educación.",
+      },
+      ResearchLine5: {
+        ResearchLineTitle: "Computación Fiable",
+        ResearchLineBody: "Esta línea se enfoca en el desarrollo de sistemas de computación fiables, abordando la tolerancia a fallos, seguridad, alta disponibilidad y rendimiento en arquitecturas distribuidas.",
+    },
+    },
+  },
+  newsEvents: {
+    title: "Noticias y Eventos",
+      titleArchive: "Archivo de Noticias y Eventos",
+    newsEventsCard: {
+      readMore: "Leer más",
+      category: {
+        event: "Evento",
+        new: "Noticia",
+        price: "Premio",
+      },
+       buttons: {
+        inscription: "Inscripción",
+        downloadArticle: "Descargar artículo",
+        web: "Ver web",
+        article: "Ver artículo",
+       },
+       archivedIn: "Archivado en:",
+    },
+    button: "Ver todas las noticias",
+  },
+  research: {
+    title: "Publicaciones",
+    description:
+      "Esta sección recopila las publicaciones del GING relacionadas con nuestras líneas de investigación. Puedes filtrar las publicaciones por texto, fecha, línea de investigación, o tipo de publicación.",
+    publicationCards: {
+      categories: {
+        "article-journal": "Artículo de revista",
+        "paper-conference": "Acta de congreso",
+        book: "Libro",
+        chapter: "Capítulo",
+        "patent":"Patente",
+        "thesis":"Tesis",
+      },
+    },
+    "action-button": "Leer publicación",
+    filter: {
+      fieldTitle1: "Buscar",
+      fieldTitle2: "Tipo de publicación",
+      fieldTitle3: "Año",
+      "article-journal":"Artículos de revista",
+      "paper-conference":"Actas de congreso",
+      "book":"Libro",
+      "chapter":"Capítulo",
+      "document":"Documento",
+      "patent":"Patente",
+      "thesis":"Tesis",
+      "all":"Todos",      
+      text: "Resultados",
+    },
+    button: "Leer publicación",
+    button2: "Ver más",
+  },
+
+  projects: {
+    title: "Proyectos",
+    description :"En esta sección puedes explorar los proyectos en los que estamos trabajando actualmente y proyectos pasados. Si lo deseas, puedes filtrar los proyectos según sus líneas de investigación o acceder a las páginas específicas de los proyectos.",
+    button: "Detalles",
+    researchLines: {
+      "data": "Datos",
+      "ai": "Inteligencia Artificial",
+      "e-learning": "E-learning",
+      "videoconference": "Videoconferencia",
+      "computing": "Computación Fiable",
+      "other": "Otros",
+      "all": "Todo",
+    },
+    "filter": {
+      "fieldTitle1":"Búsqueda por nombre",
+      "fieldTitle2":"Tipo de proyecto",
+      "all":"Todo",
+      "national-project":"Proyecto nacional",
+      "european-project":"Proyecto europeo",
+      "private-project":"Proyecto privado",
+  },
+    "card": {
+      "toggleMore": "Ver más",
+      "toggleLess": "Ver menos",
+      "button": "Publicaciones relacionadas"
+  },
+  "type": {
+    "european-project": "Proyecto europeo",
+    "national-project": "Proyecto nacional",
+    "private-project": "Proyecto privado"
+}
+  },
+  team: {
+    title: "Equipo",
+    professorCards: {
+    "roles":{
+                "professor":"Profesor Titular de Universidad",
+                "associate":"Profesor Contratado Doctor",
+                "permanentassociate":"Profesor Permanente Laboral",
+                "phd":"Candidato de Doctorado",
+                "fulltec":"Técnico a Tiempo Completo",
+                "fullprofessor":"Catedrático de Universidad", // Full Professor
+                "assistant":"Profesor Ayudante Doctor", 
+                "researcher":"Investigador",
+                "external":"Personal Externo"
+            },
+    },
+  },
+  contact: {
+    title: "Contacto",
+
+    buttonDownload: "Descargar Presentación del Ging",
+    "contactmail": {
+      "ai": "Inteligencia Artificial ",
+      "e-learning": "E-Learning",
+      "data": "Espacios de datos",
+      "videoconference": "Videoconferencia y WebRTC",
+      "computing": "Computación Fiable",
+      "general": "General"
+    
+  },
+    body: "Si tienes alguna duda o consulta relacionada con nuestras líneas de investigación, proyectos y publicaciones, o si deseas explorar posibilidades de colaboración, contáctanos a través de nuestro teléfono o los correos electrónicos indicados a continuación. ",
+    email: "Dirección de correo",
+    phone: " Teléfono",
+    fax: " Fax",
+    location: " Localización",
+    direction1: "E.T.S. DE INGENIEROS DE TELECOMUNICACIÓN",
+    direction2: "Av. Complutense, 30, Edificio B",
+    direction3: "28040 Madrid",
+  },
+
+  footer: {
+    title1: "Secciones",
+    title2: "Síguenos en:",
+    title3: "Colaboran",
+    logoSub:
+      "Redes Futuras para Centros de Datos y Empresas de Telecomunicaciones",
+    titleRight: "Departamento de Ingeniería de sistemas telemáticos",
+    titleRight2: "ETSI telecomunicación",
+    email: "Correo de contacto:",
+  },
+};
---
+++ constants/langs/3-en.js
@@ -0,0 +1,199 @@
+export const en = {
+  nav: {
+    home: "Home",
+    about: "About",
+    team: "Team",
+    research: "Research",
+    contact: "Contact",
+    designSystem: "Design system",
+    documentation: "Documentation",
+    projects: "Projects",
+  },
+  header: {
+    publicationstab: "Research",
+  },
+  front: {
+    title: "Next Generation Internet Group",
+    description:
+      "A research group from the Telematics Engineering Department (DIT) at Universidad Politécnica de Madrid (UPM). Our research is focused on educational innovation, data spaces, networks, videoconferencing, and Artificial Intelligence.",
+      "action-button": "Explore our research lines",
+    section1Title: "About us",
+    section1Description:
+      "Our main research interests focus on data space technologies, generative AI, e-Learning, and videoconferencing systems. We have extensive experience in protocol and specification design, as well as in the development of software components and use cases in these areas. We participate in competitive publicly funded research projects at the national and European level and in collaboration agreements with companies.",
+    statistics: {
+      papers: "papers",
+      citations: "citations",
+      projects: "projects",
+      
+
+    },
+    // latestPublicationsTitle: "Latest publications",
+    // latestPublicationsButton: "See all publications",
+    ResearchLines: {
+      sectionTitle: "Research lines",
+      button: "See projetcs",
+      ResearchLine1: {
+        ResearchLineTitle: "Data",
+        ResearchLineBody:
+          "This line addresses the study and development of advanced technologies for data processing, management, and analysis, including data spaces and digital twins.",
+      },
+      ResearchLine2: {
+        ResearchLineTitle: "E-Learning",
+        ResearchLineBody:
+          "This line focuses on the design and development of advanced educational technologies, including learning platforms, authoring tools, ia in education and educational escape rooms.",
+      },
+      ResearchLine3: {
+        ResearchLineTitle: "Artificial Intelligence",
+        ResearchLineBody:
+          "This line focuses on the research and development of artificial intelligence-based applications, including the evaluation of large-scale language models (LLMs), fine-tuning, and retrieval augmented generation (RAG).",
+      },
+      ResearchLine4: {
+        ResearchLineTitle: "Videoconference",
+        ResearchLineBody:
+          "This line focuses on the development of videoconferencing tools and platforms, such as Isabel and Licode, as well as the application of these technologies to interpretation and multimedia communication, improving the quality, scalability and accessibility of videoconferencing services in professional and educational environments.",
+      },
+      ResearchLine5: {
+        ResearchLineTitle: "Dependable Computing",
+        ResearchLineBody:
+          "This line focuses on the development of reliable computing systems, addressing fault tolerance, security, high availability and performance in distributed architectures.",
+      },
+    },
+  },
+ newsEvents: {
+    title: "News and events",
+    titleArchive: "News and events archive",
+    newsEventsCard: {
+      readMore: "Read more",
+      category: {
+        event: "Event",
+        new: "New",
+        price: "Prize",
+      },
+      buttons: {
+        inscription: "Inscription",
+        downloadArticle: "Download article",
+        web: "See web",
+        article: "See article",
+      },
+      archivedIn: "Filed under:",
+    },
+    button: "See all news",
+  },
+  research: {
+    title: "Research",
+    description:
+      "This section compiles the group's publications related to our research lines. You can filter the publications by text, date, research line, or type of publication.",
+    publicationCards: {
+      categories: {
+        "article-journal": "Article Journal",
+        "paper-conference": "Paper Conference",
+        book: "Book",
+        chapter: "Chapter",
+        "patent":"Patent",
+        "thesis":"Thesis"
+      },
+    },
+    "action-button": "Read publication",
+    filter: {
+      fieldTitle1: "Search",
+      fieldTitle2: "Publication type",
+      fieldTitle3: "Year",
+      "article-journal":"Article journal",
+      "paper-conference":"Conference paper",
+      "book":"Book",
+      "chapter":"Chapter",
+      "document":"Document",
+      "patent":"Patent",
+      "thesis":"Thesis",
+      "all":"All",   
+      "text":"Results"
+    },
+    button: "Read publication",
+    button2: "Load more",
+  },
+
+  projects: {
+    title: "Projects",
+    description:
+      "In this section, you can explore the projects we are currently working on as well as past projects. If you wish, you can filter the projects by their research lines or access specific project pages.",
+    filterTitle1: "All",
+    filterTitle2: "Projects - individual",
+    filterTitle3: "Educational Research Group (GIE/ERG)",
+    filterTitle4: "Other",
+    button: "Details",
+    researchLines: {
+      "data": "Data",
+      "ai": "Artificial Intelligence ",
+      "e-learning": "E-learning",
+      "videoconference": "Video Conference",
+      "computing": "Dependable Computing",
+      "other": "Other",
+      "all": "All",
+    },
+    filter: {
+      fieldTitle1: "Name search",
+      fieldTitle2: "Project type",
+      all: "All",
+      "national-project": "National Project",
+      "european-project": "European Project",
+      "private-project": "Private Project",
+    },
+    card: {
+      toggleMore: "See more",
+      toggleLess: "See less",
+      button: "Related papers",
+    },
+    type: {
+      "european-project": "European Project",
+      "national-project": "National Project",
+      "private-project": "Private Project",
+    },
+  },
+  team: {
+    title: "Team",
+    professorCards: {
+      "roles":{
+        "professor":"Professor", // Profesor Titular de Universidad
+        "associate":"Associate Professor", // Profesor Titular / Profesor contratado doctor
+        "permanentassociate":"Associate Professor", // Profesor Permanente Laboral
+        "phd":"PhD candidate", // Estudiante de doctorado
+        "fulltec":"Full-Time Technician",
+        "fullprofessor":"Professor", // Profesor Titular de Universidad
+        "assistant":"Assistant Professor",
+        "researcher":"Researcher",
+        "external":"External staff"
+    },
+    },
+  },
+  contact: {
+    title: "Contact",
+
+    buttonDownload: "Download GING presentation",
+    contactmail: {
+      ai: "Artificial Intelligence",
+      "e-learning": "E-Learning",
+      data: "Data Spaces",
+      videoconference: "Videoconferencing and WebRTC",
+      computing: "Dependable Computing",
+      general: "General",
+    },
+    body: "If you have any questions or inquiries related to our research lines, projects and publications, or want to explore collaboration opportunities, feel free to contact us via our phone or the email addresses listed below. ",
+    email: " Email address",
+    phone: " Telephone",
+    fax: "Fax",
+    location: " Location",
+    direction1: "E.T.S. DE INGENIEROS DE TELECOMUNICACIÓN",
+    direction2: "Av. Complutense, 30, Building B",
+    direction3: "28040 Madrid",
+  },
+  footer: {
+    title1: "Sections",
+    title2: "Follow us!",
+    title3: "Partners",
+    logoSub: "Future Networks for Datacenters and Telcos",
+    titleRight:
+      "Future Networks for Datacenters and Telcos (FuN4DaTe)  © 2024. All rights reserved.",
+    titleRight2: "",
+    email: "Contact Email:",
+  },
+};
